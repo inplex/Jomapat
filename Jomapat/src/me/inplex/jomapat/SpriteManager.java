c662f2ef7f3663a5263cdf81588b0efad17d5d71
matthinc <wurstbreze@gmail.com>
2014-03-09 18:26:26 +0100

    SpriteManager

---
+++ Jomapat/src/me/inplex/jomapat/SpriteManager.java
@@ -0,0 +1,31 @@
+package me.inplex.jomapat;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.imageio.ImageIO;
+
+
+
+public class SpriteManager {
+	
+	
+	//Hashmap for Sprites
+	//Integer : color in the map
+	private HashMap<Integer,BufferedImage> spriteMap = new HashMap<Integer, BufferedImage>(); 
+	
+	
+	public void loadSprite(int color,File filename){
+		BufferedImage temp = null;
+		try {
+			temp = ImageIO.read(filename);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		spriteMap.put(color, temp);
+	}
+
+}
