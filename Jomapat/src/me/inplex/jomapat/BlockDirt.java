fe5cf7fa68209e94b2ba6630272a10bd34c4e830
matthinc <wurstbreze@gmail.com>
2014-03-10 18:52:35 +0100

    BlockDirt

--- Jomapat/src/me/inplex/jomapat/BlockDirt.java
+++ Jomapat/src/me/inplex/jomapat/BlockDirt.java
@@ -1,8 +1,15 @@
 package me.inplex.jomapat;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
 
 public class BlockDirt extends Block {
+	
+	public static final byte PROP_COLLISION = 0x01;
+	public static final byte PROP_LIGHT = 0x02;
+
+	private BufferedImage sprite =  ImageIO.read(bla);
 
 	@Override
 	public byte getId() {
@@ -11,7 +18,18 @@ public class BlockDirt extends Block {
 
 	@Override
 	public BufferedImage getImage() {
-		return SpriteManager.SPRITE_BLOCK_DIRT;
+		return sprite;
 	}
+	
+	public boolean getProp(byte prop){
+		switch (prop){
+		case PROP_COLLISION:
+			return true;
+		case PROP_LIGHT:
+			return false;
+		}
+	}
+	
+	
 		
 }
