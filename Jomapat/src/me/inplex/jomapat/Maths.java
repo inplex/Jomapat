27d2a33fea2d876406b5061408cada23fc935a4c
matthinc <wurstbreze@gmail.com>
2014-03-11 17:54:26 +0100

    Fix : Math.java

---
+++ Jomapat/src/me/inplex/jomapat/Maths.java
@@ -0,0 +1,11 @@
+package me.inplex.jomapat;
+
+import java.util.Random;
+
+public class Maths {
+	
+	public static int randomize(int min, int max){
+	    Random random = new Random();
+	    return random.nextInt(max - min + 1) + min;
+	  }
+}
