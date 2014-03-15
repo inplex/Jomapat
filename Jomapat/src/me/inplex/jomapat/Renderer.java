c35147b5db3be31c76d253610f31c20323b22b6c
matthinc <wurstbreze@gmail.com>
2014-03-15 16:42:44 +0100

    center player

--- Jomapat/src/me/inplex/jomapat/Renderer.java
+++ Jomapat/src/me/inplex/jomapat/Renderer.java
@@ -23,9 +23,8 @@ public class Renderer {
 		// Jomapat.game.getPlayer().getY() + 50);
 
 		int playerX = Jomapat.game.getPlayer().getX()-Jomapat.game.getWidth()/2;
+		int playerY = Jomapat.game.getPlayer().getY()-Jomapat.game.getHeight()/2;
 		// Render Blocks
-		// TODO Improve this iteration - do not render EVERY Block, only the
-		// visible ones
 		for (int x = 0; x < Jomapat.game.getWorld().getWidth(); x++) {
 			for (int y = 0; y < Jomapat.game.getWorld().getHeight(); y++) {
 				if (Jomapat.game.getWorld().getBlockAt(x, y) == null)
@@ -33,13 +32,13 @@ public class Renderer {
 				// Render Block at
 				if (Maths.isVisible(x*SpriteManager.SPRITE_BLOCK_SIZE-playerX , y*SpriteManager.SPRITE_BLOCK_SIZE)){
 				g.drawImage(Jomapat.game.getWorld().getBlockAt(x, y).getSprite(), (x * SpriteManager.SPRITE_BLOCK_SIZE)-playerX,
-						(y * SpriteManager.SPRITE_BLOCK_SIZE), null);
+						(y * SpriteManager.SPRITE_BLOCK_SIZE)-playerY, null);
 				}
 			}
 		}
 
 		// Render Player
-		g.drawImage(Player.SPRITE_IDLE_1, Jomapat.game.getWidth()/2, Jomapat.game.getPlayer().getY(), null);
+		g.drawImage(Player.SPRITE_IDLE_1, Jomapat.game.getWidth()/2, Jomapat.game.getHeight()/2, null);
 		
 	}
 
