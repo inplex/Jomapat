c6120dec4d71763d7b22e0e6392ab1b16729cc50
Matthi <wurstbreze@gmail.com>
2014-03-25 19:30:32 +0100

    test commit

---
+++ Jomapat/src/me/inplex/enemy/Enemy.java
@@ -0,0 +1,20 @@
+package me.inplex.enemy;
+
+public class Enemy {
+	
+	public int x;
+	public int y;
+	
+	public int health;
+
+	public Enemy(int x,int y){
+		this.x=x;
+		this.y=y;
+	}
+	
+	public void move(int x,int y){
+		this.x=x;
+		this.y=y;
+	}
+	
+}
