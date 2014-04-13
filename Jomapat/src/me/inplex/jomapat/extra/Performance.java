b7f2fdf04c786737ebe98e630fce546df67365ab
inplex <christl.joshua@googlemail.com>
2014-04-13 17:46:40 +0200

    minor code improvements

--- Jomapat/src/me/inplex/jomapat/extra/Performance.java
+++ Jomapat/src/me/inplex/jomapat/extra/Performance.java
@@ -5,21 +5,19 @@ import javax.swing.JOptionPane;
 import me.inplex.jomapat.Jomapat;
 
 public class Performance {
-	
-	public static void handlePerformance(){
+
+	public static void handlePerformance() {
 
 		boolean showPrc = false;
-		if (Runtime.getRuntime().maxMemory()<300000000){showPrc = true;}
-		if (Runtime.getRuntime().availableProcessors()<3){showPrc = true;}
-		
-		if (showPrc){
-		int result = JOptionPane.showConfirmDialog(null, "Das System scheint langsam zu arbeiten. Low Performace Modus aktivieren ?",
-				"", JOptionPane.YES_NO_OPTION);
-		
-		Jomapat.game.minimalMode = (result==JOptionPane.YES_OPTION) ? true : false;
-		
+		showPrc |= Runtime.getRuntime().maxMemory() < 300000000;
+		showPrc |= Runtime.getRuntime().availableProcessors() < 3;
+
+		if (showPrc) {
+			int result = JOptionPane.showConfirmDialog(null, "Your computer seems to run slowly. Activate low performance mode?",
+					"low performance mode", JOptionPane.YES_NO_OPTION);
+			Jomapat.game.minimalMode = (result == JOptionPane.YES_OPTION);
 		}
-		
+
 	}
 
 }
--- Jomapat/src/me/inplex/jomapat/world/CaveBuilder.java
+++ Jomapat/src/me/inplex/jomapat/world/CaveBuilder.java
@@ -2,32 +2,28 @@ package me.inplex.jomapat.world;
 
 import me.inplex.jomapat.extra.Maths;
 
-public class CaveBuilder {	
-	
-	public static void buildCave(World world){
+public class CaveBuilder {
+
+	public static void buildCave(World world) {
 		int caveRandom[][] = new int[world.getWidth()][world.getHeight()];
-		for (int i=0;i<10;i++){
-			for (int x=2;x<world.getWidth()-4;x++){
-				for (int y=2;y<world.getHeight()-4;y++){
-					int random = caveRandom[x][y]==0 ? 300:caveRandom[x][y];
-					if (Maths.randomize(0, random)==1 && world.getBlockAt(x, y)==BlockType.STONE){
-						world.removeBlockAt(x, y);
-						caveRandom[x-1][y-1] = 3;
-						caveRandom[x-1][y] = 3;
-						caveRandom[x][y-1] = 3;
-						caveRandom[x+1][y+1] = 3;
-						caveRandom[x][y+1] = 3;
-						caveRandom[x+1][y] = 3;
+		for (int i = 0; i < 10; i++) {
+			for (int x = 2; x < world.getWidth() - 4; x++) {
+				for (int y = 2; y < world.getHeight() - 4; y++) {
+					int random = caveRandom[x][y] == 0 ? 300 : caveRandom[x][y];
+					if (Maths.randomize(0, random) == 1 && world.getBlockAt(x, y) == BlockType.STONE) {
+						if (world.getBlockAt(x, y) != BlockType.DIRT) {
+							world.removeBlockAt(x, y);
+							caveRandom[x - 1][y - 1] = 3;
+							caveRandom[x - 1][y] = 3;
+							caveRandom[x][y - 1] = 3;
+							caveRandom[x + 1][y + 1] = 3;
+							caveRandom[x][y + 1] = 3;
+							caveRandom[x + 1][y] = 3;
+						}
 					}
 				}
 			}
 		}
-	}					   
-
-
-
-
-
-
+	}
 
 }
